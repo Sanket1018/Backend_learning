47b00fffd3aeed56b285eac599bc25bcdd857154
Sanket <sanketkhokadesk@gmail.com>
2025-12-08 22:08:50 -0800

    adding treeset methods

--- Collection/TreeSetLaunch.java
+++ Collection/TreeSetLaunch.java
@@ -5,7 +5,7 @@ public class TreeSetLaunch {
         TreeSet ta = new TreeSet<>();
         ta.add(100);
         ta.add(25);
-        ta.add(300);
+        ta.add(200);
         ta.add(500);
         ta.add(100); // duplicate value
         System.out.println(ta);
@@ -13,6 +13,17 @@ public class TreeSetLaunch {
         // it stores the data in sorted order
         // it does not allow the duplicate values
         // it uses Binary search tree data structure
+
+        System.out.println(ta.ceiling(100)); // 100
+        System.out.println(ta.higher(100)); //200
+
+        System.out.println(ta.floor(200)); //200
+        System.out.println(ta.lower(200)); //100
+
+        System.out.println(ta.tailSet(300));
+        System.out.println(ta.headSet(300));
+
+
 
 
     }
