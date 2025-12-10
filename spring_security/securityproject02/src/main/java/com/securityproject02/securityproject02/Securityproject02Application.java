592541dcdc7c93f6ec51a065f5a131c1aa5a2942
Sanket <sanketkhokadesk@gmail.com>
2025-12-10 01:09:05 -0800

    Adding spring security code

---
+++ spring_security/securityproject02/src/main/java/com/securityproject02/securityproject02/Securityproject02Application.java
@@ -0,0 +1,13 @@
+package com.securityproject02.securityproject02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityproject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securityproject02Application.class, args);
+	}
+
+}
