0fc5fd70c4251c3212183811026f26c2e931a943
Sanket <sanketkhokadesk@gmail.com>
2025-12-19 13:24:40 -0800

    Adding synchronized code of multithreading

--- Multithreading/LaunchingSynchronize.java
+++ Multithreading/LaunchingSynchronize.java
@@ -1,3 +1,51 @@
 public class LaunchingSynchronize {
-    
+
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        Thread t1 = new Thread(c);
+        Thread t2 = new Thread(c);
+        Thread t3 = new Thread(c);
+
+        t1.setName("Sanket");
+        t2.setName("Akash");
+        t3.setName("Rohan");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+   
+    }
+
 }
+
+    class Car implements Runnable{
+
+        synchronized public void run()
+        {
+            System.out.println(Thread.currentThread().getName()+" is entered into the parking lot");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+" is entering into the car");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+" has started driving the car");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+     
+
+
+
+    }
